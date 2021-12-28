fe328404d439444bd84f331cdca317c9f12f6208
zhangpeng <zhangpeng_vip@hotmail.com>
2021-12-28 21:20:01 +0800

    Add AppCenter for logging and crash report

--- Source/AppCenterDemo/App.xaml.cs
+++ Source/AppCenterDemo/App.xaml.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Xamarin.Essentials;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
 
 namespace AppCenterDemo
 {
     public partial class App : Application
     {
+        public const string LogTag = "AppCenterXamarinDemo";
+
         public App()
         {
             InitializeComponent();
@@ -15,6 +19,13 @@ namespace AppCenterDemo
 
         protected override void OnStart()
         {
+            AppCenter.LogLevel = LogLevel.Verbose;
+            Crashes.ShouldAwaitUserConfirmation = UserConfirmationHandler;
+            AppCenter.IsNetworkRequestsAllowed = Preferences.Get("Diagnostic", false);
+            AppCenter.Start(
+                "ios=96145684-8c4f-4637-8827-4f0a4e1c08a1;" +
+                "android=1b6fa4c8-e136-4e1f-8193-48723aa7c919",
+                typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
@@ -23,6 +34,65 @@ namespace AppCenterDemo
 
         protected override void OnResume()
         {
+        }
+
+        bool UserConfirmationHandler()
+        {
+            Xamarin.Forms.Device.BeginInvokeOnMainThread(() =>
+            {
+                var enabled = Preferences.Get("Diagnostic", false);
+                if (enabled)
+                {
+                    var networkAccess = Connectivity.NetworkAccess;
+                    if (networkAccess != NetworkAccess.Internet)
+                    {
+                        Current.MainPage.DisplayAlert(
+                            "Notice",
+                            "Failed to send crash report, please check your Internet connection.",
+                            "OK");
+                    }
+                    Crashes.NotifyUserConfirmation(UserConfirmation.Send);
+                }
+                else
+                {
+                    Current.MainPage.DisplayActionSheet("Crash detected. Send anonymous crash report?", null, null,
+                        "Send",
+                        "Always Send",
+                        "Don't Send").ContinueWith((arg) =>
+                        {
+                            var answer = arg.Result;
+                            UserConfirmation userConfirmationSelection;
+                            if (answer == "Send")
+                            {
+                                AppCenter.IsNetworkRequestsAllowed = true;
+                                userConfirmationSelection = UserConfirmation.Send;
+                            }
+                            else if (answer == "Always Send")
+                            {
+                                Preferences.Set("Diagnostic", true); // Remember this settings by App, instead of AppCenter
+                                AppCenter.IsNetworkRequestsAllowed = true;
+                                userConfirmationSelection = UserConfirmation.Send;
+                            }
+                            else
+                            {
+                                userConfirmationSelection = UserConfirmation.DontSend;
+                            }
+                            if (userConfirmationSelection == UserConfirmation.Send)
+                            {
+                                var networkAccess = Connectivity.NetworkAccess;
+                                if (networkAccess != NetworkAccess.Internet)
+                                {
+                                    Xamarin.Forms.Device.BeginInvokeOnMainThread(() => Current.MainPage.DisplayAlert(
+                                        "Notice",
+                                        "Internet connection unavailable, please check your Internet connection.",
+                                        "OK"));
+                                }
+                            }
+                            Crashes.NotifyUserConfirmation(userConfirmationSelection);
+                        });
+                }
+            });
+            return true;
         }
     }
 }
--- Source/AppCenterDemo/MainPage.xaml.cs
+++ Source/AppCenterDemo/MainPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace AppCenterDemo
@@ -13,6 +15,59 @@ namespace AppCenterDemo
         public MainPage()
         {
             InitializeComponent();
+
+            var enabled = Preferences.Get("Diagnostic", false);
+            this.SendDiagnostic = enabled;
+
+            Xamarin.Forms.Device.BeginInvokeOnMainThread(async () => await Update_AppCenter_Status());
+        }
+
+        private async void Write_Log_Button_Clicked(object sender, EventArgs e)
+        {
+            System.Guid? installId = await AppCenter.GetInstallIdAsync();
+            var now = DateTime.Now;
+
+            var trackEventCount = Preferences.Get("TrackEventCount", 0);
+            ++trackEventCount;
+            Preferences.Set("TrackEventCount", trackEventCount);
+
+            Analytics.TrackEvent("Track Event A", new Dictionary<string, string>
+            {
+                { "InstallId", installId?.ToString() },
+                { "TrackEventCount", trackEventCount.ToString() },
+                { "Time", now.ToString() }
+            });
+        }
+
+        private void Crash_It_Button_Clicked(object sender, EventArgs e)
+        {
+            throw new Exception("Crash it at " + DateTime.Now);
+        }
+
+        public bool SendDiagnostic
+        {
+            get => Preferences.Get("Diagnostic", false);
+            set
+            {
+                Preferences.Set("Diagnostic", value);
+                AppCenter.IsNetworkRequestsAllowed = value;
+            }
+        }
+
+        private async void Check_Status_Button_Clicked(object sender, EventArgs e)
+        {
+            await Update_AppCenter_Status();
+        }
+
+        private async Task Update_AppCenter_Status()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine($"AppCenter.IsEnabled = {await AppCenter.IsEnabledAsync()}");
+            sb.AppendLine($"AppCenter.IsNetworkRequestsAllowed = {AppCenter.IsNetworkRequestsAllowed}");
+            sb.AppendLine($"AppCenter.LogLevel = {AppCenter.LogLevel}");
+            sb.AppendLine($"Crashes.IsEnabled = {await Crashes.IsEnabledAsync()}");
+            sb.AppendLine($"Analytics.IsEnabled = {await Analytics.IsEnabledAsync()}");
+            this.AppCenterStatus.Text = sb.ToString();
         }
     }
 }
